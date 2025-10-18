852d8965e12a7f7698e7482bb08d91000bc7c829
Frank <mail@franku.nl>
2025-10-18 22:25:20 +0200

    Add descriptions to clap commands

--- src/commands.rs
+++ src/commands.rs
@@ -2,7 +2,8 @@ use std::path::PathBuf;
 use clap::{Parser, Subcommand, Args};
 
 #[derive(Parser, Debug)]
-#[command(name = "Packit", version, about = "Universal package manager", long_about = None)]
+#[command(name = "Packit", version, about)]
+#[command(long_about = "The universal package manager, designed to streamline the experience of installing packages on your system.")]
 pub struct Cli {
     #[command(subcommand)]
     command: Commands,
@@ -10,25 +11,32 @@ pub struct Cli {
 
 #[derive(Subcommand, Debug)]
 enum Commands {
+    /// Install a package on your system
     Install(InstallArgs),
+    /// Uninstall a package from your system
     Uninstall(UninstallArgs),
+    /// List all installed packages
     List(ListArgs),
 }
 
 #[derive(Args, Debug)]
 struct InstallArgs {
+    /// The name of the package to install
     package_name: String,
 
+    /// The version of the package to install
     #[arg(short, long)]
-    version: Option<String>
+    version: Option<String>,
 }
 
 #[derive(Args, Debug)]
 struct UninstallArgs{
+    /// The name of the package to uninstall
     package_name: String,
 }
 
 #[derive(Args, Debug)]
 struct ListArgs {
+    /// Directory to list all packages of (OPTIONAL)
     directory: Option<PathBuf>,
 }
