2e62ee458ff08155d2a7bb37d4b73bc61aaffe63
Max <maxrijkers@outlook.com>
2025-10-20 20:26:34 +0200

    add other basic target architectures

--- src/target_architecture.rs
+++ src/target_architecture.rs
@@ -9,4 +9,24 @@ pub const TARGET_ARCHITECTURE: &str = "aarch64-apple-darwin";
 #[cfg(target_os = "linux")]
 #[cfg(target_arch = "aarch64")]
 #[cfg(target_env = "gnu")]
-pub const TARGET_ARCHITECTURE: &str = "unknown-linux-gnu";
+pub const TARGET_ARCHITECTURE: &str = "aarch64-unknown-linux-gnu";
+
+#[cfg(target_os = "linux")]
+#[cfg(target_arch = "x86_64")]
+#[cfg(target_env = "gnu")]
+pub const TARGET_ARCHITECTURE: &str = "x86_64-unknown-linux-gnu";
+
+#[cfg(target_os = "linux")]
+#[cfg(target_arch = "x86_64")]
+#[cfg(target_env = "musl")]
+pub const TARGET_ARCHITECTURE: &str = "x86_64-unknown-linux-musl";
+
+#[cfg(target_os = "windows")]
+#[cfg(target_arch = "x86_64")]
+#[cfg(target_env = "msvc")]
+pub const TARGET_ARCHITECTURE: &str = "x86_64-pc-windows-msvc";
+
+#[cfg(target_os = "windows")]
+#[cfg(target_arch = "aarch64")]
+#[cfg(target_env = "msvc")]
+pub const TARGET_ARCHITECTURE: &str = "aarch64-pc-windows-msvc";
