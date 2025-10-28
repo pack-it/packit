8da5e0abeba65994190c53054b7159d3368d31bf
Frank <mail@franku.nl>
2025-10-28 14:27:17 +0100

    Add repositories command

--- src/cli/commands.rs
+++ src/cli/commands.rs
@@ -1,8 +1,10 @@
 use clap::{Args, Parser, Subcommand};
+use colored::Colorize;
 use std::path::PathBuf;
 use thiserror::Error;
 
 use crate::{
+    cli::{self},
     config::Config,
     installed_packages::{InstalledPackageStorage, InstalledPackagesError},
     installer::{error::InstallerError, installer::Installer},
@@ -40,6 +42,9 @@ enum Commands {
 
     /// List all installed packages
     List(ListArgs),
+
+    /// List all configured repositories
+    Repositories,
 }
 
 #[derive(Args, Debug)]
@@ -83,6 +88,7 @@ pub fn handle_command(manager: &RepositoryManager, config: &Config) -> Result<()
         Commands::Install(args) => Installer::new(&config, &mut installed_storage).install(manager, &args.package_name, args.version)?,
         Commands::Uninstall(args) => Installer::new(&config, &mut installed_storage).uninstall(&args.package_name, args.version)?,
         Commands::List(args) => handle_list(args, &installed_storage, &config)?,
+        Commands::Repositories => handle_repositories(config, manager),
     }
 
     // Save changes
@@ -104,4 +110,33 @@ fn handle_list(args: ListArgs, installed_storage: &InstalledPackageStorage, conf
     }
 
     Ok(())
+}
+
+/// Handles the repositories command, listing all configured repositories.
+fn handle_repositories(config: &Config, manager: &RepositoryManager) {
+    let mut first = true;
+
+    for (repository_id, repository) in &config.repositories {
+        if !first {
+            println!();
+        }
+        first = false;
+
+        // Read metadata of repository
+        let metadata = match manager.read_repository_metadata(&repository_id) {
+            Ok(metadata) => metadata,
+            Err(e) => {
+                // Display the error and continue
+                cli::display_warning(&format!("Cannot read repository metadata of repository '{repository_id}'"));
+                cli::display_warning(&format!("{e}"));
+                continue;
+            },
+        };
+
+        // Print repository information
+        println!("{} ({repository_id})", metadata.name.bold().blue());
+        println!("{}", metadata.description.green());
+        println!("Maintainers: {}", metadata.maintainers.join(", "));
+        println!("Repository provider: {}, path: {}", repository.provider, repository.path);
+    }
 }
