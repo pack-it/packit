23d6cdc0f02b44ffa56095eecb072c7983ee6d1b
Max Rijkers <max_rijkers1@icloud.com>
2026-01-25 23:01:54 +0100

    add unwrap or exit message support

--- src/error_handling.rs
+++ src/error_handling.rs
@@ -3,10 +3,21 @@ use std::{error::Error, process::exit};
 use crate::cli::display::logging::error;
 
 pub trait HandleError<T> {
+    fn unwrap_or_exit_msg(self, msg: &str, exit_code: i32) -> T;
     fn unwrap_or_exit(self, exit_code: i32) -> T;
 }
 
 impl<T, E: Error> HandleError<T> for Result<T, E> {
+    fn unwrap_or_exit_msg(self, msg: &str, exit_code: i32) -> T {
+        match self {
+            Ok(value) => value,
+            Err(e) => {
+                error!(e, msg);
+                exit(exit_code);
+            },
+        }
+    }
+
     fn unwrap_or_exit(self, exit_code: i32) -> T {
         match self {
             Ok(value) => value,
