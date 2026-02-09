ebba9df888d530bfe7061ed7314350a728953a7b
Max Rijkers <max_rijkers1@icloud.com>
2026-02-10 00:28:00 +0100

    Add check to check if the given package exists

--- src/cli/commands/package.rs
+++ src/cli/commands/package.rs
@@ -1,9 +1,14 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, process::exit};
 
 use clap::Args;
 
 use crate::{
-    cli::commands::HandleCommand, config::Config, installer::types::PackageId, packager::package, repositories::manager::RepositoryManager,
+    cli::{commands::HandleCommand, display::logging::error},
+    config::Config,
+    installer::types::PackageId,
+    packager::package,
+    repositories::manager::RepositoryManager,
+    storage::package_register::PackageRegister,
     utils::unwrap_or_exit::UnwrapOrExit,
 };
 
@@ -18,6 +23,15 @@ pub struct PackageArgs {
 
 impl HandleCommand for PackageArgs {
     fn handle(&self, config: &Config, manager: &RepositoryManager) {
+        let register_dir = PackageRegister::get_default_path();
+        let register = PackageRegister::from(&register_dir).unwrap_or_exit(1);
+
+        // Throw an error if the specified package doesn't exist
+        if register.get_package_version(&self.package_id).is_none() {
+            error!(msg: "Cannot package package, because the given package does not exist.");
+            exit(1);
+        }
+
         package(config, &self.package_id, &self.destination, manager).unwrap_or_exit(1);
     }
 }
