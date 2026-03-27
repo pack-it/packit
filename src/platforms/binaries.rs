d86d03d95550ee08669863e01422f9e1cbf467c0
Frank <mail@franku.nl>
2026-03-28 00:00:37 +0100

    Implement MachO patching

--- src/platforms/binaries.rs
+++ src/platforms/binaries.rs
@@ -1,9 +1,13 @@
-use std::{collections::VecDeque, fs, path::PathBuf};
+use std::{collections::VecDeque, fs, path::PathBuf, process::Command};
 
 use lief::{Binary, elf, macho};
 use thiserror::Error;
 
-use crate::{cli::display::logging::debug, config::Config};
+use crate::{
+    cli::display::logging::{debug, error},
+    config::Config,
+    installer::types::PackageId,
+};
 
 /// The errors that occur during binary operations.
 #[derive(Error, Debug)]
@@ -21,6 +25,9 @@ pub enum BinaryError {
 
     #[error("Error while interacting with filesystem")]
     IOError(#[from] std::io::Error),
+
+    #[error("Cannot convert OsString to string")]
+    OsStringConversionError,
 }
 
 type Result<T> = std::result::Result<T, BinaryError>;
@@ -34,18 +41,17 @@ impl<'a> BinaryPatcher<'a> {
         Self { config }
     }
 
-    pub fn patch_binaries_in(&self, path: &PathBuf) -> Result<()> {
+    pub fn patch_binaries_in(&self, path: &PathBuf, package: &PackageId) -> Result<()> {
         let metadata = fs::metadata(path)?;
 
         // If the given path is a file, patch the file directly
         if metadata.is_file() {
-            return self.patch_binary(path);
+            return self.patch_binary(path, package);
         }
 
         let mut queue = VecDeque::from([path.clone()]);
         while queue.len() != 0 {
             let item = queue.pop_front().expect("Expected an element in the queue");
-            debug!("Searching for binaries in '{}'", item.display());
 
             for entry in fs::read_dir(item)? {
                 let entry = entry?;
@@ -59,7 +65,7 @@ impl<'a> BinaryPatcher<'a> {
 
                 // If the entry is a file, try to patch it
                 if metadata.is_file() {
-                    match self.patch_binary(&entry.path()) {
+                    match self.patch_binary(&entry.path(), package) {
                         Ok(_) | Err(BinaryError::CannotParseBinary { .. }) | Err(BinaryError::UnsupportedBinaryType { .. }) => (),
                         Err(e) => return Err(e),
                     }
@@ -70,15 +76,15 @@ impl<'a> BinaryPatcher<'a> {
         Ok(())
     }
 
-    fn patch_binary(&self, path: &PathBuf) -> Result<()> {
+    fn patch_binary(&self, path: &PathBuf, package: &PackageId) -> Result<()> {
         match Binary::parse(path) {
             Some(Binary::ELF(binary)) => {
                 debug!("Patching ELF binary at '{}'", path.display());
-                self.patch_elf(binary, path)?;
+                self.patch_elf(binary, path, package)?;
             },
             Some(Binary::MachO(binary)) => {
                 debug!("Patching MachO binary at '{}'", path.display());
-                self.patch_macho(binary, path)?;
+                self.patch_macho(binary, path, package)?;
             },
             Some(Binary::PE(_binary)) => {
                 debug!("Patching PE binary at '{}'", path.display());
@@ -96,34 +102,77 @@ impl<'a> BinaryPatcher<'a> {
         Ok(())
     }
 
-    fn patch_macho(&self, binary: macho::FatBinary, path: &PathBuf) -> Result<()> {
+    fn patch_macho(&self, binary: macho::FatBinary, path: &PathBuf, package: &PackageId) -> Result<()> {
         for mut binary in binary.iter() {
             let mut changed = false;
 
-            for library in binary.libraries() {
+            for mut library in binary.libraries() {
                 let library_path = PathBuf::from(library.name());
 
                 // Check if library lives in the prefix directory
-                if library_path.starts_with(&self.config.prefix_directory) {
+                let prefix = self.config.prefix_directory.join("packages/");
+                if library_path.starts_with(&prefix) {
                     debug!("Found Packit dependency: {}", library_path.display());
 
-                    // TODO: split out package name and link to correct directory
-                    // TODO: check if the library links to a path outside of the current package
-                    // changed = true;
+                    let library_path = match library_path.strip_prefix(&prefix) {
+                        Ok(path) => path,
+                        Err(_) => {
+                            debug!("Cannot remove prefix from dependency path");
+                            continue;
+                        },
+                    };
+
+                    let parts: Vec<_> = library_path.components().collect();
+                    if parts.len() < 2 {
+                        debug!("Linked dependency path is too short");
+                        continue;
+                    }
+
+                    let dependency_name = parts[0].as_os_str().to_str().ok_or(BinaryError::OsStringConversionError)?;
+                    let dependency_version = parts[1].as_os_str().to_str().ok_or(BinaryError::OsStringConversionError)?;
+
+                    // Check if the dependency links to the package itself
+                    if dependency_name == *package.name && dependency_version == package.version.to_string() {
+                        continue;
+                    }
+
+                    let new_prefix = self.config.prefix_directory.join("dependencies").join(package.to_string()).join(dependency_name);
+                    let new_suffix: PathBuf = parts.iter().skip(2).collect();
+                    let new_path = new_prefix.join(new_suffix);
+
+                    library.set_name(new_path.to_str().ok_or(BinaryError::OsStringConversionError)?);
+
+                    changed = true;
                 }
             }
 
             if changed {
+                debug!("Changed binary {path:?}, writing changes");
+
                 let mut config = macho::builder::Config::default();
                 config.linkedit = true;
                 binary.write_with_config(path, config);
+
+                // Sign binary
+                let path = path.to_str().ok_or(BinaryError::OsStringConversionError)?;
+                match Command::new("/usr/bin/codesign").args(["--sign", "-", "--force", path]).status() {
+                    Ok(code) if !code.success() => {
+                        error!(msg: "Cannot resign binary, exit code {code}");
+                        continue;
+                    },
+                    Ok(_) => (),
+                    Err(e) => {
+                        error!(e, "Cannot resign binary {path:?}");
+                        continue;
+                    },
+                };
             }
         }
 
         Ok(())
     }
 
-    fn patch_elf(&self, binary: elf::Binary, path: &PathBuf) -> Result<()> {
+    fn patch_elf(&self, binary: elf::Binary, path: &PathBuf, package: &PackageId) -> Result<()> {
         todo!();
         Ok(())
     }
