d8b5dc1ec8f3f626d58e3a41154ba427ed4add33
Frank <mail@franku.nl>
2026-04-19 20:09:45 +0200

    Add EditableConfig using toml_edit

--- src/cli/commands/config.rs
+++ src/cli/commands/config.rs
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0-only
 use clap::Subcommand;
 
-use crate::{cli::commands::HandleCommand, config::Config, utils::unwrap_or_exit::UnwrapOrExit};
+use crate::{
+    cli::commands::HandleCommand,
+    config::{Config, EditableConfig},
+    utils::unwrap_or_exit::UnwrapOrExit,
+};
 
 /// Manages the Packit config file.
 #[derive(Subcommand, Debug)]
@@ -12,7 +16,7 @@ pub enum ConfigArgs {
 
 impl HandleCommand for ConfigArgs {
     fn handle(&self) {
-        let config = Config::from(&Config::get_default_path()).unwrap_or_exit_msg("Cannot load config", 1);
+        let config = EditableConfig::from(&Config::get_default_path()).unwrap_or_exit_msg("Cannot load config", 1);
 
         match self {
             ConfigArgs::Show => self.handle_show(config),
@@ -21,16 +25,16 @@ impl HandleCommand for ConfigArgs {
 }
 
 impl ConfigArgs {
-    fn handle_show(&self, config: Config) {
-        println!("Prefix directory: {}", config.prefix_directory.display());
+    fn handle_show(&self, config: EditableConfig) {
+        println!("Prefix directory: {}", config.get_prefix_directory().display());
 
         print!("Multiuser mode: ");
-        if config.multiuser {
+        if config.is_multiuser() {
             println!("on");
         } else {
             println!("off");
         }
 
-        println!("Repositories rank: {}", config.repositories_rank.join(", "));
+        println!("Repositories rank: {}", config.get_repositories_rank().join(", "));
     }
 }
