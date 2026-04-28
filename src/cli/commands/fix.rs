5aa38e5059f26a5bcf0311a9eb5d2f25496977b8
Max Rijkers <max_rijkers1@icloud.com>
2026-04-28 15:06:30 +0200

    Add support for checking specific packages with the fix command

--- src/cli/commands/fix.rs
+++ src/cli/commands/fix.rs
@@ -1,4 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-only
+use std::path::Path;
+
 use clap::Args;
 
 use crate::{
@@ -7,15 +9,19 @@ use crate::{
         display::{QuestionResponse, ask_user},
     },
     config::Config,
+    installer::types::PackageId,
     repositories::manager::RepositoryManager,
     storage::package_register::PackageRegister,
     utils::unwrap_or_exit::UnwrapOrExit,
-    verifier::{Repairer, Verifier},
+    verifier::{Issue, Repairer, Verifier},
 };
 
 /// Fixes all issues found by the check command. The user will be asked if they want to fix an issue for each issue type.
 #[derive(Args, Debug)]
-pub struct FixArgs;
+pub struct FixArgs {
+    /// A vec of packages to fix. Could be empty, then all packages are fixed.
+    pub packages: Vec<PackageId>,
+}
 
 impl HandleCommand for FixArgs {
     fn handle(&self) {
@@ -24,32 +30,61 @@ impl HandleCommand for FixArgs {
         let register_dir = PackageRegister::get_default_path(&config);
         let mut register = PackageRegister::from(&register_dir).unwrap_or_exit(1);
         let mut verifier = Verifier::new(&config);
-
         let mut repairer = Repairer::new(&config, &manager);
 
+        match self.packages.is_empty() {
+            true => self.fix_all(&mut verifier, &mut repairer, &mut register, &register_dir),
+            false => self.fix_packages(&mut verifier, &mut repairer, &mut register, &register_dir),
+        }
+
+        // Save changes
+        register.save_to(&register_dir).unwrap_or_exit(1);
+    }
+}
+
+impl FixArgs {
+    /// Fixes all packages.
+    fn fix_all(&self, verifier: &mut Verifier, repairer: &mut Repairer, register: &mut PackageRegister, register_dir: &Path) {
         // Retrieve and fix the issues one by one
-        while let Some(issue) = verifier.next_issue(&register).unwrap_or_exit(1) {
-            println!("{issue}");
-
-            println!("{}", issue.get_fix_message());
-            let question = "Would you like to automatically apply the fix above?";
-            if ask_user(question, QuestionResponse::Yes).unwrap_or_exit(1).is_no() {
-                continue;
-            }
-
-            // Repair the found issues
-            repairer.fix(issue, &mut register).unwrap_or_exit(1);
-
-            // Save register after the fix
-            register.save_to(&register_dir).unwrap_or_exit(1);
+        while let Some(issue) = verifier.next_issue(register).unwrap_or_exit(1) {
+            self.fix_issue(issue, repairer, register, register_dir);
         }
 
         // Return correct message based on found issues
         if !verifier.issues_found() {
             println!("No issues were found");
         }
+    }
 
-        // Save changes
+    /// Fixes the packages specified by the user.
+    fn fix_packages(&self, verifier: &mut Verifier, repairer: &mut Repairer, register: &mut PackageRegister, register_dir: &Path) {
+        for package_id in &self.packages {
+            // Retrieve and fix the issues one by one
+            while let Some(issue) = verifier.next_package_issue(package_id, register).unwrap_or_exit(1) {
+                self.fix_issue(issue, repairer, register, register_dir);
+            }
+
+            // Show when no errors are found for the current package
+            if !verifier.issues_found() {
+                println!("No issues were found for {package_id}");
+            }
+        }
+    }
+
+    /// Fixes a specific issue.
+    fn fix_issue(&self, issue: Issue, repairer: &mut Repairer, register: &mut PackageRegister, register_dir: &Path) {
+        println!("{issue}");
+
+        println!("{}", issue.get_fix_message());
+        let question = "Would you like to automatically apply the fix above?";
+        if ask_user(question, QuestionResponse::Yes).unwrap_or_exit(1).is_no() {
+            return;
+        }
+
+        // Repair the found issues
+        repairer.fix(issue, register).unwrap_or_exit(1);
+
+        // Save register after each fix
         register.save_to(&register_dir).unwrap_or_exit(1);
     }
 }
