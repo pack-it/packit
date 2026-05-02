1c93421c1c3379996319c3ec3403156118b63bd3
Frank <mail@franku.nl>
2026-05-02 21:55:41 +0200

    Implement generation of index.toml and downloading of external test files for portable repo

--- src/repositories/types/index.rs
+++ src/repositories/types/index.rs
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0-only
 use std::collections::HashSet;
 
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 use crate::installer::types::PackageName;
 
 /// An index of all supported packages in the repository
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct IndexMeta {
     pub supported_packages: HashSet<PackageName>,
 }
