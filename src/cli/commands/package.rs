3b488187f8e7242f50b989be3936944ff7954479
Max Rijkers <max_rijkers1@icloud.com>
2026-05-11 21:06:33 +0200

    Allow multiple package arguments in package command

--- src/cli/commands/package.rs
+++ src/cli/commands/package.rs
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0-only
-use std::{fs, path::PathBuf};
+use std::{fs, path::PathBuf, process::exit};
 
 use clap::Args;
 
 use crate::{
     cli::{
         commands::HandleCommand,
-        display::{Spinner, not_found},
+        display::{Spinner, logging::error, not_found},
     },
     config::Config,
     installer::types::PackageId,
@@ -18,11 +18,11 @@ use crate::{
 /// Packages the specified package into a prebuild and store it in the destination directory, together with a checksum of the prebuild.
 #[derive(Args, Debug)]
 pub struct PackageArgs {
-    /// The package id of the package
-    pub package_id: PackageId,
-
     /// Destination of the compressed package
     pub destination: PathBuf,
+
+    /// The package id of the package
+    pub packages: Vec<PackageId>,
 
     /// True to sort the package into a prebuild directory
     #[arg(short, long, default_value = "false")]
@@ -35,30 +35,43 @@ impl HandleCommand for PackageArgs {
         let register_dir = PackageRegister::get_default_path(&config.prefix_directory);
         let register = PackageRegister::from(&register_dir).unwrap_or_exit(1);
 
-        let package_version = match register.get_package_version(&self.package_id) {
-            Some(package_version) => package_version,
-            None => not_found::register_package_version(&self.package_id, &register),
-        };
+        if self.packages.is_empty() {
+            error!(msg: "Nothing packaged, no packages specified");
+            exit(1);
+        }
 
-        // Get the correct install directory
-        let destination = match self.sorted {
-            true => {
-                let prefix = self.package_id.name.get_prefix().to_string();
-                &self.destination.join("packages").join(prefix).join(&self.package_id.name).join(self.package_id.version.to_string())
-            },
-            false => &self.destination,
+        for package_id in &self.packages {
+            // Get the correct install directory
+            let destination = match self.sorted {
+                true => {
+                    let prefix = package_id.name.get_prefix().to_string();
+                    &self.destination.join("packages").join(prefix).join(&package_id.name).join(package_id.version.to_string())
+                },
+                false => &self.destination,
+            };
+
+            self.package(package_id, destination, &config, &register);
+        }
+    }
+}
+
+impl PackageArgs {
+    fn package(&self, package_id: &PackageId, destination: &PathBuf, config: &Config, register: &PackageRegister) {
+        let package_version = match register.get_package_version(package_id) {
+            Some(package_version) => package_version,
+            None => not_found::register_package_version(package_id, &register),
         };
 
         // Automatically create the destination directory
         fs::create_dir_all(destination).unwrap_or_exit_msg("Failed to create prebuild directory", 1);
 
         let spinner = Spinner::new();
-        let spinner_message = format!("Packaging {}", self.package_id);
+        let spinner_message = format!("Packaging {package_id}");
         spinner.show(spinner_message.clone());
 
-        packager::package(&config, &self.package_id, destination, package_version.revisions.len() as u64).unwrap_or_exit(1);
+        packager::package(&config, package_id, destination, package_version.revisions.len() as u64).unwrap_or_exit(1);
 
         spinner.finish(format!("{spinner_message} successful"));
-        println!("Successfully packaged {} to {:?}", self.package_id, self.destination);
+        println!("Successfully packaged {package_id} to {:?}", destination);
     }
 }
