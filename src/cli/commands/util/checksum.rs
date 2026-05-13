8e1a91cb4409c67c2d0ea9977a6f0ad915baff21
Frank <mail@franku.nl>
2026-05-14 00:04:18 +0200

    Split util command into separate modules

---
+++ src/cli/commands/util/checksum.rs
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: GPL-3.0-only
+use std::process::exit;
+
+use clap::Args;
+use url::Url;
+
+use crate::{
+    cli::{
+        commands::HandleCommand,
+        display::{Spinner, logging::error},
+    },
+    repositories::types::Checksum,
+    utils::requests,
+};
+
+/// Calculates the checksum for the file at the given url
+#[derive(Args, Debug)]
+pub struct ChecksumArgs {
+    /// The url to request the file from
+    url: Url,
+}
+
+impl HandleCommand for ChecksumArgs {
+    fn handle(&self) {
+        println!("Trying to request file from {}", self.url);
+
+        let spinner = Spinner::new();
+        spinner.show("Downloading file".into());
+
+        let response = match requests::get(self.url.as_str()) {
+            Ok(response) => response,
+            Err(e) => {
+                spinner.finish("Downloading file unsuccessful".into());
+                error!(e, "Unable to request file");
+                exit(1);
+            },
+        };
+
+        if !response.status().is_success() {
+            spinner.finish("Downloading file unsuccessful".into());
+            error!(msg: "File request returned status code {}", response.status().as_u16());
+            exit(1);
+        }
+
+        let bytes = match response.bytes() {
+            Ok(bytes) => bytes,
+            Err(e) => {
+                spinner.finish("Downloading file unsuccessful".into());
+                error!(e, "Unable to get file bytes");
+                exit(1);
+            },
+        };
+
+        let checksum = Checksum::from_bytes(&bytes);
+
+        spinner.finish("Downloading file successful".into());
+        println!("Found checksum {}", checksum.to_string());
+    }
+}
---
+++ src/cli/commands/util/portable_repo.rs
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: GPL-3.0-only
+use std::path::PathBuf;
+
+use clap::Args;
+
+use crate::{
+    cli::{commands::HandleCommand, display::Spinner},
+    config::Config,
+    installer::types::PackageId,
+    platforms::Target,
+    register::package_register::PackageRegister,
+    repositories::{manager::RepositoryManager, portable_repo::PortableRepoCreator},
+    utils::unwrap_or_exit::UnwrapOrExit,
+};
+
+/// Generates a portable repository containing the specified packages
+#[derive(Args, Debug)]
+pub struct PortableRepoArgs {
+    /// The destination directory
+    destination: PathBuf,
+
+    /// The packages to include in the portable repository
+    packages: Vec<PackageId>,
+
+    /// True to exclude prebuilds in the portable repository
+    #[arg(long, default_value = "false")]
+    exclude_prebuilds: bool,
+
+    /// True to skip automatic addition of dependencies to the portable repository
+    #[arg(long, default_value = "false")]
+    skip_dependency_resolution: bool,
+}
+
+impl HandleCommand for PortableRepoArgs {
+    fn handle(&self) {
+        let config = Config::from(&Config::get_default_path()).unwrap_or_exit_msg("Cannot load config", 1);
+        let manager = RepositoryManager::new(&config);
+        let register =
+            PackageRegister::from(&PackageRegister::get_path(&config.prefix_directory)).unwrap_or_exit_msg("Cannot load register", 1);
+
+        let spinner = Spinner::new();
+        spinner.show("Generating portable repository".into());
+
+        let creator = PortableRepoCreator::new(
+            &config,
+            &manager,
+            &register,
+            Target::current(),
+            self.exclude_prebuilds,
+            self.skip_dependency_resolution,
+        );
+
+        creator
+            .create_portable_repo(self.packages.iter().cloned().collect(), &self.destination)
+            .unwrap_or_exit_msg("Cannot create portable repository", 1);
+
+        spinner.finish("Generating portable repository successful".into());
+        println!("Created portable repository at {}!", self.destination.display());
+    }
+}
