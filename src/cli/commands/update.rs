a53e0ecebd97c70e577e39acedb3d3e634d62286
Max Rijkers <max_rijkers1@icloud.com>
2026-07-02 10:05:31 +0200

    Allow multiple package arguments to the update command

--- src/cli/commands/update.rs
+++ src/cli/commands/update.rs
@@ -7,6 +7,7 @@ use crate::{
     cli::{
         commands::HandleCommand,
         display::{logging::error, not_found},
+        parameter_checks,
     },
     config::Config,
     installer::{
@@ -24,9 +25,10 @@ use crate::{
 #[derive(Args, Debug)]
 pub struct UpdateArgs {
     /// The name of the package to update, with an optional version specified with NAME@VERSION
-    optional_id: OptionalPackageId,
+    optional_ids: Vec<OptionalPackageId>,
 
-    /// The version to update to. This can only be a higher version than the current version
+    /// The version to update to. This can only be a higher version than the current version and is only used when a single package is specified
+    #[arg(long, requires = "optional_ids")]
     new_version: Option<Version>,
 }
 
@@ -37,35 +39,63 @@ impl HandleCommand for UpdateArgs {
         let register_dir = PackageRegister::get_path(&config.prefix_directory);
         let mut register = PackageRegister::from(&register_dir).unwrap_or_exit(1);
 
-        match self.optional_id.versioned() {
-            Some(package_id) if register.get_package_version(&package_id).is_some() => {},
-            Some(package_id) => not_found::register_package_version(&package_id, &register),
-            None if register.get_package(&self.optional_id.name).is_some() => {},
-            None => not_found::register_package(&self.optional_id.name, &register),
-        }
-
-        let (_, package_meta) = manager.read_package(&self.optional_id.name).unwrap_or_exit(1);
-
-        let new_version = match &self.new_version {
-            Some(version) => version,
-            None => package_meta.get_latest_version(&Target::current()).unwrap_or_exit(1),
-        };
-
-        // Check if the new version exists
-        if !package_meta.versions.contains(new_version) {
-            error!(msg: "New package version '{new_version}' does not exist.");
-            not_found::repository_version(&self.optional_id.name, &manager);
+        if self.optional_ids.len() > 1 && self.new_version.is_some() {
+            error!(msg: "Cannot specify new version when multiple packages are given");
             exit(1);
         }
 
-        let options = InstallerOptions::default();
-        let mut installer = Installer::new(&config, &mut register, &manager, options);
+        // Check for duplicates, because updating twice will result in an error
+        let duplicates = parameter_checks::get_duplicates(&self.optional_ids);
+        if !duplicates.is_empty() {
+            let mut duplicate_string = String::new();
+            for duplicate in duplicates {
+                duplicate_string.push_str(&duplicate.to_string());
+                duplicate_string.push(' ');
+            }
 
-        let new_package_id = installer.update(&self.optional_id, new_version).unwrap_or_exit(1);
+            error!(msg: "Duplicate package arguments are not allowed. The following duplicates were found: {duplicate_string}");
+            exit(1);
+        }
 
-        println!("Successfully updated {} to {new_package_id}", self.optional_id);
+        for optional_id in &self.optional_ids {
+            match optional_id.versioned() {
+                Some(package_id) if register.get_package_version(&package_id).is_some() => {},
+                Some(package_id) => not_found::register_package_version(&package_id, &register),
+                None if register.get_package(&optional_id.name).is_some() => {},
+                None => not_found::register_package(&optional_id.name, &register),
+            }
 
-        // Save changes
-        register.save_to(&register_dir).unwrap_or_exit(1);
+            let (_, package_meta) = manager.read_package(&optional_id.name).unwrap_or_exit(1);
+
+            // Note that there is a check earlier which checks if the new_version is `None` in case of multiple packages
+            let new_version = match &self.new_version {
+                Some(version) => version,
+                None => package_meta.get_latest_version(&Target::current()).unwrap_or_exit(1),
+            };
+
+            // Check if the new version exists
+            if !package_meta.versions.contains(new_version) {
+                error!(msg: "New package version '{new_version}' does not exist.");
+                not_found::repository_version(&optional_id.name, &manager);
+                exit(1);
+            }
+
+            let options = InstallerOptions::default();
+            let mut installer = Installer::new(&config, &mut register, &manager, options);
+
+            // Do the update, and in case of an error throw the error, but continue
+            let new_package_id = match installer.update(&optional_id, new_version) {
+                Ok(new_package_id) => new_package_id,
+                Err(error) => {
+                    error!(error, "Cannot update package {optional_id}");
+                    continue;
+                },
+            };
+
+            println!("Successfully updated {} to {new_package_id}", optional_id);
+
+            // Save changes
+            register.save_to(&register_dir).unwrap_or_exit(1);
+        }
     }
 }
