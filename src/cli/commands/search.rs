2b16d798b025f7a7889608dd44108ebb9aa6c3e7
Max Rijkers <max_rijkers1@icloud.com>
2026-07-11 20:35:21 +0200

    Show different search information based on optional id

--- src/cli/commands/search.rs
+++ src/cli/commands/search.rs
@@ -13,20 +13,20 @@ use crate::{
             deprecation,
             logging::error,
             not_found,
-            standard_print::{self},
+            standard_print::{self, DisplayJoined, DisplayOption},
             styled::{MapStyled, Styled},
         },
     },
     config::Config,
-    installer::types::OptionalPackageId,
+    installer::types::{OptionalPackageId, PackageId, PackageName},
     platforms::Target,
     repositories::{error::RepositoryError, manager::RepositoryManager},
     utils::unwrap_or_exit::UnwrapOrExit,
 };
 
-/// Searches a package with `<PACKAGE-NAME>` and shows information based on the package metadata.
+/// Searches a package with `<PACKAGE-NAME>[@<PACKAGE-VERSION]` and shows information based on the package (version) metadata.
 /// Alternatively, when the regex flag is true, it uses the regex query to search for packages which match the regex.
-/// If the version is given that specific version is searched for.
+/// Package version specific information is shown when the version is given, otherwise package specific information is shown.
 #[derive(Args, Debug)]
 pub struct SearchArgs {
     /// The query to search with (can be an `OptionalPackageId` or regex string)
@@ -42,7 +42,7 @@ impl HandleCommand for SearchArgs {
     fn handle(&self) {
         match self.regex {
             true => self.regex_search(),
-            false => self.search_package(),
+            false => self.search(),
         }
     }
 }
@@ -76,18 +76,24 @@ impl SearchArgs {
 
     /// Searches information of a package based on the provided `OptionalPackageId`.
     /// Fails if the given query is not a valid `OptionalPackageId`.
-    fn search_package(&self) {
+    fn search(&self) {
         // Get the optional id
         let message = "The given search query isn't a valid package. For regex use `--regex`.";
         let optional_id = OptionalPackageId::from_str(&self.query).unwrap_or_exit_msg(message, 1);
 
+        match optional_id.versioned() {
+            Some(package_id) => self.search_package_version(&package_id),
+            None => self.search_package(&optional_id.name),
+        }
+    }
+
+    /// Searches for and shows package specific information for a given package.
+    fn search_package(&self, package_name: &PackageName) {
         let config = Config::from(&Config::get_default_path()).unwrap_or_exit_msg("Cannot load config", 1);
         let manager = RepositoryManager::new(&config);
-        let (repository_id, package, package_version) = match manager.read_package_and_version(&optional_id, &Target::current()) {
+        let (repository_id, package) = match manager.read_package(package_name) {
             Ok(package) => package,
-            Err(RepositoryError::PackageNotFoundError { reason, .. }) => {
-                not_found::repository_optional_package(&optional_id, &manager, reason)
-            },
+            Err(RepositoryError::PackageNotFoundError { reason, .. }) => not_found::repository_package(package_name, &manager, reason),
             Err(e) => {
                 error!(e, "Cannot read package");
                 return;
@@ -107,8 +113,39 @@ impl SearchArgs {
             },
         };
 
-        // Create a package id
-        let package_id = optional_id.versioned_or_cloned(&package_version.version);
+        // Print package information
+        println!("{}", package_name.style());
+        println!("{}", package.description.italic().cyan());
+        let mut pair_aligner = PairAligner::new();
+        pair_aligner.add("Homepage", package.homepage.display());
+        pair_aligner.add("Latest version", latest_version.version.style());
+        pair_aligner.add("Available versions", package.versions.iter().map_styled().display(" | "));
+        pair_aligner.add("Required Packit version", package.required_packit_version.display().bold().blue());
+        pair_aligner.display(PairAligner::VERTICAL_LINE_PREFIX);
+        println!();
+
+        print!("Conflicts with: ");
+        standard_print::print_list_or_none(package.conflicts_with.iter());
+
+        // Check if the package is deprecated
+        deprecation::show_package_warnings(&package);
+    }
+
+    /// Searches for and shows package version specific information for a given package.
+    fn search_package_version(&self, package_id: &PackageId) {
+        let config = Config::from(&Config::get_default_path()).unwrap_or_exit_msg("Cannot load config", 1);
+        let manager = RepositoryManager::new(&config);
+        let package_and_version = manager.read_package_and_version(&package_id.clone().into(), &Target::current());
+        let (repository_id, package, package_version) = match package_and_version {
+            Ok(package) => package,
+            Err(RepositoryError::PackageNotFoundError { reason, .. }) => {
+                not_found::repository_package_version(&package_id, &manager, reason)
+            },
+            Err(e) => {
+                error!(e, "Cannot read package");
+                return;
+            },
+        };
 
         let target_bounds = package_version.get_best_target(&Target::current()).unwrap_or_exit(1);
 
@@ -121,27 +158,33 @@ impl SearchArgs {
             },
         };
 
-        let dependencies: Vec<_> = package_version.dependencies.iter().chain(target.dependencies.iter()).collect();
-        let dependencies: Vec<String> = dependencies.iter().map(|d| d.to_string()).collect();
+        // Chain the package version dependencies with the target dependencies
+        let dependencies = package_version.dependencies.iter().chain(target.dependencies.iter());
+        let build_dependencies = package_version.build_dependencies.iter().chain(target.build_dependencies.iter());
 
-        // Print package information
-        let styled_package = format!("{}@{}", package.name, package_version.version).bold().blue();
-        println!("{styled_package}");
+        let required_packit_version = package_version.required_packit_version.display().bold().blue();
+
+        // Show package version information
+        println!("{}", package_id.style());
         println!("{}", package.description.italic().cyan());
         let mut pair_aligner = PairAligner::new();
-        pair_aligner.add("Latest stable version", latest_version.version.style());
+        pair_aligner.add("Homepage", package.homepage.display());
         pair_aligner.add("License", &package_version.license);
+        pair_aligner.add("Required Packit version", required_packit_version);
+        pair_aligner.add("Skip symlinking", &package_version.skip_symlinking);
         pair_aligner.display(PairAligner::VERTICAL_LINE_PREFIX);
         println!();
 
         print!("Dependencies: ");
-        standard_print::print_list_or_none(dependencies.iter());
+        standard_print::print_list_or_none(dependencies);
+
+        print!("Build dependencies: ");
+        standard_print::print_list_or_none(build_dependencies);
 
         print!("Revisions: ");
         standard_print::print_list_or_none(package_version.revisions.iter());
 
-        // Check if the package or version is deprecated
-        deprecation::show_package_warnings(&package);
-        deprecation::show_package_version_warnings(&package_version, &optional_id.name);
+        // Check if the package is deprecated
+        deprecation::show_package_version_warnings(&package_version, &package_id.name);
     }
 }
