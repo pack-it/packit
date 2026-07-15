eb6733fb3f0af4dba39c99180d053d9fc19ff49b
Frank <mail@franku.nl>
2026-07-15 20:00:41 +0200

    Fix MSVC detection

--- src/platforms/tool_detection/windows.rs
+++ src/platforms/tool_detection/windows.rs
@@ -1,32 +1,35 @@
 // SPDX-License-Identifier: GPL-3.0-only
 use std::{path::PathBuf, process::Command};
 
-use crate::{platforms::tool_detection::error::Result, utils::ioerror::IOResultExt};
+use crate::{cli::display::logging::debug, platforms::tool_detection::error::Result, utils::ioerror::IOResultExt};
 
 /// Detects if MSVC is installed on the system.
 /// Returns the path to vcvarsall.bat if it is found, or `None` if MSVC is not found.
 pub fn detect_msvc() -> Result<Option<PathBuf>> {
     // Check if `vswhere` exists
-    let vswhere = PathBuf::from("C:\\Program Files (x86)\\Microsoft Visual Studio\\Installer\\vswhere");
+    let vswhere = PathBuf::from("C:\\Program Files (x86)\\Microsoft Visual Studio\\Installer\\vswhere.exe");
     if !vswhere.exists() {
+        debug!("Cannot find 'vswhere.exe'");
         return Ok(None);
     }
 
     // Read Visual Studio install path from `vswhere`
     let mut command = Command::new(vswhere);
-    command.args(["-latest", "-property installationPath"]);
+    command.args(["-latest", "-property", "installationPath"]);
     let output = command.output().err_operation("run vswhere command")?;
     let path = str::from_utf8(&output.stdout)?;
-    let path = PathBuf::from(path);
+    let path = PathBuf::from(path.trim());
 
     // Check if install path exists
     if !path.exists() {
+        debug!("The Visual Studio install does not exist at '{}'", path.display());
         return Ok(None);
     }
 
     // Check if vcvarsall.bat exists
     let vcvarsall = path.join("VC").join("Auxiliary").join("Build").join("vcvarsall.bat");
     if !vcvarsall.exists() {
+        debug!("The vcvarsall.bat script does not exist at '{}'", vcvarsall.display());
         return Ok(None);
     }
 
