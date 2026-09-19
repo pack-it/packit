62085a8983bb6430dc8eab885047b5ba94d93669
Max Rijkers <max_rijkers1@icloud.com>
2026-09-19 21:13:08 +0200

    Small refactor of duplicate code

--- src/installer/installer.rs
+++ src/installer/installer.rs
@@ -74,7 +74,7 @@ impl<'a> Installer<'a> {
 
         // Make sure that repositories already used for installed packages which are not in the `Config.toml` anymore
         // don't conflict with the current repository
-        let conflicts = self.repository_manager.repository_conflicts_with(self.config, self.register.get_repository_names())?;
+        let conflicts = self.repository_manager.repository_conflicts_with(self.config, self.register.iterate_repository_names())?;
         if !conflicts.is_empty() {
             return Err(InstallerError::IncompatibleRepositories { conflicts });
         }
